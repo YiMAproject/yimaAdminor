ef106f2234eaf164e686300d8d2b4a08f807392d
Payam Naderi <naderi.payam@gmail.com>
2014-06-19 23:08:16 +0430

    add default adminor navigation menus

--- config/module.navigation.config.php
+++ config/module.navigation.config.php
@@ -1,47 +1,21 @@
 <?php
 return array(
     'admin' => array(
-        'Application' => array(
-            'label' 	 => 'Application',
-            'id'	 	 => 'admin_nav_application',
+        array(
+            'label' 	 => 'Adminor',
             'route'		 => \yimaAdminor\Module::ADMIN_ROUTE_NAME,
-            'order' 	 => -10000,
+            'order' 	 => 100000,
             'pages' 	 => array(
-                'dashboard'  => array(
-                    'label' 	 => 'Adminor Dashboard',
-                    'icon'		 => 'icon-flatscreen',
+                array(
+                    'label' 	 => 'Dashboard',
                     'route'		 => \yimaAdminor\Module::ADMIN_ROUTE_NAME,
                     'order' 	 => -10000,
                 ),
-                'website'  => array(
-                    'label' 	 => 'Application Admin',
-                    'module'	 => 'Application',
-                    #'controller' => 'Index', // as default
-                    #'action'	 =>	'dashboard', // as default
-                    'params'	 => array('this' => 'that'),
-                    'icon'		 => 'icon-flatscreen',
+                array(
+                    'label' 	 => 'Logout',
+                    'route'		 => 'yima_adminor_auth/logout',
+                    'order' 	 => -10000,
                 ),
-                'Home'  => array(
-                    'label' 	 => 'Web Site Home',
-                    'module'	 => 'Application',
-                    'route'		 => 'home',
-                    'icon'		 => 'icon-flatscreen',
-                ),
-                'rayaOnline' => array(
-                    'label'  => 'Raya Online',
-                    'id' 	 => 'raya-online',
-                    'uri' 	 => 'http://www.raya-media.com/cms',
-                    'target' => '_blank',
-                    'icon'	 => 'icon-speech',
-                    'order'  => 10000,
-                ),
-            ),
-        ),
-        'Modules' => array(
-            'label'  => 'Modules',
-            'uri'	 => '#',
-            'id'	 => 'admin_nav_modules',
-            'pages'  => array(
             ),
         ),
     ),
