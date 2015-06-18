a35332548f2e301e7071d25ac174467811e22403
Payam Naderi <naderi.payam@gmail.com>
2015-06-18 15:29:34 +0430

    fix config keys

--- config/module.navigation.config.php
+++ config/module.navigation.config.php
@@ -1,20 +1,22 @@
 <?php
 return [
-    'admin' => [
-        [
-            'label' 	 => 'Adminor',
-            'route'		 => \yimaAdminor\Module::ADMIN_ROUTE_NAME,
-            'order' 	 => -100000,
-            'pages' 	 => [
-                [
-                    'label' 	 => 'Dashboard',
-                    'route'		 => \yimaAdminor\Module::ADMIN_ROUTE_NAME,
-                    'order' 	 => -10000,
-                ],
-                [
-                    'label' 	 => 'Logout',
-                    'route'		 => 'yima_adminor_auth/logout',
-                    'order' 	 => -10000,
+    'navigation' => [
+        'admin' => [
+            [
+                'label' 	 => 'Adminor',
+                'route'		 => \yimaAdminor\Module::ADMIN_ROUTE_NAME,
+                'order' 	 => -100000,
+                'pages' 	 => [
+                    [
+                        'label' 	 => 'Dashboard',
+                        'route'		 => \yimaAdminor\Module::ADMIN_ROUTE_NAME,
+                        'order' 	 => -10000,
+                    ],
+                    [
+                        'label' 	 => 'Logout',
+                        'route'		 => 'yima_adminor_auth/logout',
+                        'order' 	 => -10000,
+                    ],
                 ],
             ],
         ],
--- config/module.route.config.php
+++ config/module.route.config.php
@@ -1,61 +1,63 @@
 <?php
 return [
-    'routes' => [
-        # Authentication
-        'yima_adminor_auth' => [
-            'type'    => 'Literal',
-            'options' => [
-                'route'    => '/adminor',
-                'defaults' => [
-                    'controller'   => 'yimaAdminor\Controller\Account',
-                    'action'       => 'login',
-                ],
-            ],
-            'may_terminate' => true,
-            'child_routes' => [
-                'logout' => [
-                    'type'    => 'Literal',
-                    'options' => [
-                        'route' => '/logout',
-                        'defaults' 	 => [
-                            'action'     => 'logout',
-                        ],
+    'router' => [
+        'routes' => [
+            # Authentication
+            'yima_adminor_auth' => [
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => [
+                    'route'    => '/adminor',
+                    'defaults' => [
+                        'controller'   => 'yimaAdminor\Controller\Account',
+                        'action'       => 'login',
                     ],
-                    'may_terminate' => true,
                 ],
-            ],
-        ],
-        # Admin Area Routes
-        \yimaAdminor\Module::ADMIN_ROUTE_NAME => [
-            'type'    => 'Literal',
-            'options' => [
-                # /admin
-                'route'    => '/'.\yimaAdminor\Module::ADMIN_ROUTE_SEGMENT,
-                'defaults' => [
-                    'module' 	   => 'yimaAdminor', // also you can use zend approach __NAMESPACE__ as alternate
-                    'controller'   => 'Index',
-                    'action'       => 'dashboard',
-                ],
-            ],
-            'may_terminate' => true,
-            'child_routes' => [
-                'default' => [
-                    'type'    => 'yimaAdminor\Mvc\Router\Http\Crypto', // use class exists
-                    'options' => [
-                        # we can use any word after /admin[/word]/ to browse child routes
-                        'route' => '/', //'/browse/',
-                        'defaults' 	 => [
-                            'controller' => 'Index',
-                            'action'     => 'dashboard',
+                'may_terminate' => true,
+                'child_routes' => [
+                    'logout' => [
+                        'type'    => 'Literal',
+                        'options' => [
+                            'route' => '/logout',
+                            'defaults' 	 => [
+                                'action'     => 'logout',
+                            ],
                         ],
-                        # we can pass any options to Router
-                        // 'option' => 'value',
+                        'may_terminate' => true,
+                    ],
+                ],
+            ],
+            # Admin Area Routes
+            \yimaAdminor\Module::ADMIN_ROUTE_NAME => [
+                'type'    => 'Literal',
+                'options' => [
+                    # /admin
+                    'route'    => '/'.\yimaAdminor\Module::ADMIN_ROUTE_SEGMENT,
+                    'defaults' => [
+                        'module' 	   => 'yimaAdminor', // also you can use zend approach __NAMESPACE__ as alternate
+                        'controller'   => 'Index',
+                        'action'       => 'dashboard',
+                    ],
+                ],
+                'may_terminate' => true,
+                'child_routes' => [
+                    'default' => [
+                        'type'    => 'yimaAdminor\Mvc\Router\Http\Crypto', // use class exists
+                        'options' => [
+                            # we can use any word after /admin[/word]/ to browse child routes
+                            'route' => '/', //'/browse/',
+                            'defaults' 	 => [
+                                'controller' => 'Index',
+                                'action'     => 'dashboard',
+                            ],
+                            # we can pass any options to Router
+                            // 'option' => 'value',
 
-                        # encrypt and decrypt class name or object() to de/encode queries for Crypto router
-                        # 'cryption' => '\yimaAdminor\Mvc\Router\Http\Crypto\CryptionBase64', // by default
-                        # 'cryption' => '\yimaAdminor\Mvc\Router\Http\Crypto\CryptionDebuging', // Debug Mode(None Encoded)
+                            # encrypt and decrypt class name or object() to de/encode queries for Crypto router
+                            # 'cryption' => '\yimaAdminor\Mvc\Router\Http\Crypto\CryptionBase64', // by default
+                            # 'cryption' => '\yimaAdminor\Mvc\Router\Http\Crypto\CryptionDebuging', // Debug Mode(None Encoded)
+                        ],
+                        'may_terminate' => true,
                     ],
-                    'may_terminate' => true,
                 ],
             ],
         ],
